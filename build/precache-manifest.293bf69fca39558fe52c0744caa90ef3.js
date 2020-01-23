e1216bc7f23b67dc738411704d81742b98221193
Evgeniy Kononko <evgeniy.escape@gmail.com>
2020-01-23 02:25:49 +0200

    add production

---
+++ build/precache-manifest.293bf69fca39558fe52c0744caa90ef3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e1323b09d813685f9c216203b8a623",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcdc740abc06d019c23c",
+    "url": "/static/js/2.d0142ad5.chunk.js"
+  },
+  {
+    "revision": "15187a587559f992e9ad1212c52739b3",
+    "url": "/static/js/2.d0142ad5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e288aadab12aae568064",
+    "url": "/static/js/main.0a48a8da.chunk.js"
+  },
+  {
+    "revision": "ea221473f0fe65d5878d",
+    "url": "/static/js/runtime-main.fa62ab57.js"
+  }
+]);
